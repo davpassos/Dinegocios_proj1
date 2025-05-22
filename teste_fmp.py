b4ee21ffa0ceef226db9d988d65f8015b6727b6e
Davi Dutra da Silva Passos <134224775+davpassos@users.noreply.github.com>
2025-05-22 01:39:07 +0000

    Inclusão da chave api fmp

--- teste_fmp.py
+++ teste_fmp.py
@@ -1,5 +1,5 @@
 import requests
-api_key = "SUA_CHAVE_AQUI"  # Substitua pela chave da FMP
+api_key = "EXJdqaeDxTfNvCKIc73ke4Go05xGV8kn"  # Substitua pela chave da FMP
 symbol = "PETR4.SA"
 url = f"https://financialmodelingprep.com/api/v3/profile/{symbol}?apikey={api_key}"
 response = requests.get(url)
